18dad101aeddd5655bbd75a77ccb6f2067be6ca8
Rishabh-Dev-Mishra <rishabhdevmishra388@gmail.com>
2026-03-19 13:07:51 +0530

    create backend

---
+++ auth-backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.send("Welcome");
+})
+
+app.listen(3000, (req, res)=>{
+    console.log("Server Is Running");
+})

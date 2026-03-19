78d201e6448e0c560ee666a491246bbcb0cfde07
Rishabh-Dev-Mishra <rishabhdevmishra388@gmail.com>
2026-03-19 15:18:01 +0530

    Add register and login route

--- auth-backend/server.js
+++ auth-backend/server.js
@@ -10,6 +10,21 @@ app.get("/", (req, res)=>{
     res.send("Welcome");
 })
 
+app.post("/register", async(req, res)=>{
+    const {firstname, lastname, email, password} = req.body;
+    await pool.query("Insert INTO users(firstname, lastname, email, password) values ($1, $2, $3, $4)",
+        [firstname, lastname, email, password])
+        res.json({message: "Registered"})
+});
+
+app.post("/login", async (req, res)=>{
+    const {email, password} = req.body;
+    const user = await pool.query("Select * from users where email = $1", [email]);
+    if(user.rows.length === 0)
+            return res.status(400).send("User not found");
+    res.json({message: "Login Success"});
+});
+
 app.listen(3000, (req, res)=>{
     console.log("Server Is Running");
 })

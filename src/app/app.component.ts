11eaa9357aa1f2ada3401c599df9c75cc55622e8
Rishabh-Dev-Mishra <rishabhdevmishra388@gmail.com>
2026-03-20 18:07:30 +0530

    Add icon and profile component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { TodoComponent } from './todo/todo.component';
 import { routeGuardGuard } from './route-guard.guard';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
     {path: "", component : LoginComponent},
     {path: "register", component: RegisterComponent},
-    {path: "home", component: TodoComponent, canActivate: [routeGuardGuard]}
+    {path: "home", component: TodoComponent, canActivate: [routeGuardGuard]},
+    {path: "profile", component: ProfileComponent}
 ];

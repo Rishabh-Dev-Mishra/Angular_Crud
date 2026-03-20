e74bec186c42a9b063bf2d81a9337d9db14f2135
Rishabh-Dev-Mishra <rishabhdevmishra388@gmail.com>
2026-03-20 12:28:13 +0530

    Added Todo Component

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-todo',
+  imports: [FooterComponent],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css'
+})
+export class TodoComponent {
+
+}

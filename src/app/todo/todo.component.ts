51f1fd0af58bc5e7133af1efae042758f8598215
Rishabh-Dev-Mishra <rishabhdevmishra388@gmail.com>
2026-03-23 11:09:17 +0530

    Create profile page

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-todo',
-  imports: [FooterComponent],
+  imports: [FooterComponent, RouterLink, NgIf],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
-
+  imageURL: string = "https://media.newyorker.com/photos/59095bb86552fa0be682d9d0/master/pass/Monkey-Selfie.jpg";
+  
 }

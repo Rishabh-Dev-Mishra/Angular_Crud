f378bc109f483fb383349c53152bb7e9dd802577
Rishabh-Dev-Mishra <rishabhdevmishra388@gmail.com>
2026-03-27 11:36:09 +0530

    create tables

--- src/app/brands/brands.component.ts
+++ src/app/brands/brands.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-brands',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, RouterLink],
   templateUrl: './brands.component.html',
   styleUrl: './brands.component.css'
 })

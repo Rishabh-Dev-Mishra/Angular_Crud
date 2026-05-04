78948c84ddf9dd04d7682f51381d229088fef3e5
Rishabh-Dev-Mishra <rishabhdevmishra388@gmail.com>
2026-05-04 15:59:55 +0530

    Added margin for button in allcars

--- auth-backend/db.js
+++ auth-backend/db.js
@@ -1,19 +1,19 @@
 const { Pool } = require("pg");
 require("dotenv").config({ path: "../.env" });
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
 // const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database:'authdb',
-//   password: 'postgres',
-//   port:5432
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
 // });
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database:'authdb',
+  password: 'postgres',
+  port:5432
+});
 
 module.exports = pool;
